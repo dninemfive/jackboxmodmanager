abf63378b8db7e8648ba4aa4d271a41c0b16a7bb
dninemfive <11084010+dninemfive@users.noreply.github.com>
2022-11-02 12:46:20 -0700

    work on logic; switch to .NET 5.0

---
+++ Mod.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JackboxModManager
+{
+    public class Mod
+    {
+        public string Name;
+        public string Description;
+        public readonly bool VanillaGame;
+        public Mod(string name, string description = null, bool vanilla = false)
+        {
+            Name = name;
+            Description = description;
+            VanillaGame = vanilla;
+        }
+    }
+    public record JackboxVersion
+    {
+        public readonly int GameNumber;
+        public readonly int Build;
+        public JackboxVersion(int gameNumber, int build)
+        {
+            GameNumber = gameNumber;
+            Build = build;
+        }
+    }
+}
