fe8db73e51431d1b6f9ddefea18ef68aecc2b2df
dninemfive <11084010+dninemfive@users.noreply.github.com>
2022-11-02 13:54:22 -0700

    i don't know what i'm doing anymore. i think i have a goal in mind but it eludes me when i look for it. rarely does it all *not* feel pointless and that is, to say the least, very difficult to bear.
    this also applies to this repository.

--- ModInfo.cs
+++ ModInfo.cs
@@ -1,6 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Collections.Generic;
 
 namespace JackboxModManager
 {
@@ -11,12 +12,13 @@ namespace JackboxModManager
         public readonly bool VanillaGame;
         public List<JackboxVersion> JackboxVersions = new();
         public string BaseFolder;
-        public ModInfo(string name, string description = null, bool vanilla = false, params JackboxVersion[] jackboxVersions)
+        public ModInfo(string name, string description = null, string baseFolder = null, bool vanilla = false, params JackboxVersion[] jackboxVersions)
         {
             Name = name;
             Description = description;
+            BaseFolder = baseFolder ?? $"{MainWindow.CurrentConfig.ModFolder}/{name}";
             VanillaGame = vanilla;
-
+            JackboxVersions = jackboxVersions.ToList();
         }
     }
     public record ModVersion : IComparable
